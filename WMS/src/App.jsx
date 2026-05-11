9f761899d32d8d66d08ba8ef643cf00593680812
Selva Bharath P <selvabharathp@S4CARLISLE.NET>
2026-05-11 10:00:16 +0530

    case

--- WMS/src/App.jsx
+++ WMS/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/login";
-import Signup from "./pages/signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 import Administrator from "./pages/Administrator";
 import Reader from "./pages/Reader";
